d62aa61f1bbb0f47cc400ff46c62a4354297f363
Colin McKechney <colin@mckechney.com>
2024-04-22 11:57:05 -0700

    Removed name from noemo

--- src/modules/noemo.rs
+++ src/modules/noemo.rs
@@ -1,16 +1,13 @@
 use irc::proto::Message;
 use std::collections::VecDeque;
 
-pub const PATTERN: &str = "^\\$noemo (?P<nick>[^\\s]+)";
+pub const PATTERN: &str = "^\\$noemo\\s*$";
 pub const NAME: &str = "noemo";
 pub const USAGE: &str = "Usage: $noemo <nick>\r\nThis tells the user identified by nick to not be emo";
 
-pub fn no_emo(captures: regex::Captures, message: &Message, _: &VecDeque<Message>) -> String {
-    let emo_person = captures.get(1).unwrap().as_str();
-
-    let complete_message = format!("{} thinks you shouldn't be so emo, {}. Take a deep breath and lighten up",
-                                   message.source_nickname().unwrap_or("unknown_nick").to_string(),
-                                   emo_person);
+pub fn no_emo(_: regex::Captures, message: &Message, _: &VecDeque<Message>) -> String {
+    let complete_message = format!("{} thinks you shouldn't be so emo. Take a deep breath and lighten up",
+                                   message.source_nickname().unwrap_or("unknown_nick").to_string());
     complete_message
 }
 

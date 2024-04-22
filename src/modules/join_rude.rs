20f04bc3c1b8788a8fdb184474bd1eeb30924092
Colin McKechney <colin@mckechney.com>
2024-04-22 12:10:25 -0700

    Robbit now ignore's it's own join, added extra insults

--- src/modules/join_rude.rs
+++ src/modules/join_rude.rs
@@ -1,13 +1,11 @@
 use rand::prelude::Rng;
 
-const PHRASES:[&str;24] = [
+const PHRASES:[&str;26] = [
     "Go back to your cronjobs",
     "Don't you have a student machine to fork bomb?",
-    "If your poor excuse for a bot is GPT4 then I'm Alan Turing",
     "You put semicolons in your python code",
     "You put your curly brackets on a separate line",
     "Just stop now, you're not getting the guru point",
-    "OS is going to eat you alive",
     "L + Ratio",
     "You probably think it makes more sense to index at 1",
     "Matlab user",
@@ -18,21 +16,30 @@ const PHRASES:[&str;24] = [
     "Wow another bot that responds when you say hello, how unique",
     "Zahm may be closed, but you still give off Zahm vibes",
     "They really let anyone in here huh?",
-    "You're the tech bro who doesn't actually know how to code",
-    "You wear a sweater when it's 70 outside",
+    "You're the kind of tech bro who doesn't actually know how to code",
     "Go take a shower I can smell you through the screen",
     "Don't try to hard you'll hurt yourself",
     "I know fish with better coding skills than you",
     "Nepo baby",
-    "this student uses LIGHT MODE in vscode"
+    "this student uses light mode in vscode",
+    "Go home",
+    "I thought I kicked you",
+    "You forgot to respond to pings",
+    "Just get a bouncer already",
+    "Aren't you supposed to be in class?",
+
 ];
 
 pub fn join_rude(nick: &str, channel: &str) -> Option<(String,String)> {
+    if nick == "robbit" {
+        None
+    }
+    else {
+        let rude_message: &str = PHRASES[rand::thread_rng().gen_range(0..PHRASES.len())];
+        let complete_message: String = nick.to_string() + ": " + rude_message;
 
-    let rude_message: &str = PHRASES[rand::thread_rng().gen_range(0..PHRASES.len())];
-    let complete_message: String = nick.to_string() + ": " + rude_message;
-
-    Some((channel.to_string(), complete_message))
+        Some((channel.to_string(), complete_message))
+    }
 }
 
 
